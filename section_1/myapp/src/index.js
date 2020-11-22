5fcf8ad222fc7dd5279e7bb7c9bc7c5af1042ab0
Dylan Bedetti <dylanbedetti@gmail.com>
2020-11-22 13:54:00 +0000

    started section 2

--- section_1/myapp/src/index.js
+++ section_1/myapp/src/index.js
@@ -4,7 +4,18 @@ import ReactDOM from "react-dom";
 
 // Create a react componnet
 const App = function () {
-  return <div>Hii there!</div>;
+  // return <div>Hello there!</div>;
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        Submit
+      </button>
+    </div>
+  );
 };
 
 // Take the react components and show it on the screen
