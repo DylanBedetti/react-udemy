4f7eb6b840fc7b56d35fa56b3b26ea3c54f72b81
Dylan Bedetti <dylanbedetti@gmail.com>
2020-11-23 13:56:02 +0000

    Starting section 6

--- section_4/seasons/src/index.js
+++ section_4/seasons/src/index.js
@@ -3,16 +3,36 @@ import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css";
 
 class App extends React.Component {
-  render() {
+  // constructor is javascript - runs first before anything else
+  constructor(props) {
+    // refernce to the parents (react.components) constructor function
+    // what are the props here used for??
+    super(props);
+
+    // becomes avaliale throughout any other functions
+    this.state = { lat: null, long: null, errorMessage: "" };
+
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        console.log(position);
+        this.setState({ lat: position.coords.latitude });
+        this.setState({ long: position.coords.longitude });
       },
       (err) => {
-        console.log(err);
+        this.setState({ errorMessage: err.message });
       }
     );
-    return <div>Latitude: </div>;
+  }
+
+  render() {
+      if (this.state.errorMessage && !this.state.lat) {
+        return <div>Error: {this.state.errorMessage}</div>;
+      }
+
+      if (!this.state.errorMessage && this.state.lat) {
+        return <div>Latitude: {this.state.lat}</div>;
+      }
+
+      return <div>Loading...</div>;
   }
 }
 
