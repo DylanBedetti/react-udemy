33e29133631c8307972a37524d53d6399877436e
Dylan Bedetti <dylanbedetti@gmail.com>
2020-12-08 10:35:29 +0000

    19

--- section_18/blog/src/actions/index.js
+++ section_18/blog/src/actions/index.js
@@ -1,5 +1,9 @@
-export const fetchPosts = () => {
-  return {
-    type: "FETCH_POSTS",
-  };
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+// changed syntax a bit
+// defining a function that is going to return a function
+export const fetchPosts = async () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
+
+  dispatchEvent({ type: "FETCH_POSTS", payload: response });
 };
--- section_18/blog/src/index.js
+++ section_18/blog/src/index.js
@@ -1,21 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from "./components/App";
 import reducers from "./reducers";
 
 import "semantic-ui-css/semantic.min.css";
 
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(thunk),
+));
+
+
 ReactDOM.render(
-  <Provider
-    store={createStore(
-      reducers,
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )}
-  >
+  <Provider store={store}>
     <App />
   </Provider>,
   document.querySelector("#root")
