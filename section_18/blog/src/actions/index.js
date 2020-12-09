b00245a786a8e5cb7befe95037a37ead0d16e2b6
Dylan Bedetti <dylanbedetti@gmail.com>
2020-12-09 06:01:01 +0000

    finished 19

--- section_18/blog/src/actions/index.js
+++ section_18/blog/src/actions/index.js
@@ -1,9 +1,39 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts()); // waiting until this is complete until we progress to next line
+
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach(id => dispatch(fetchUser(id))) // don't need await, because no other logic 
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value() //execute 
+};
 
 // changed syntax a bit
 // defining a function that is going to return a function
-export const fetchPosts = async () => async (dispatch) => {
+export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
 
-  dispatchEvent({ type: "FETCH_POSTS", payload: response });
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
+
+// export const fetchUser = (id) => (dispatch) => {
+//   _fetchUser(id, dispatch);
+// };
+// // underscore means private function
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
