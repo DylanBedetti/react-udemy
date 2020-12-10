a00285cf37448d6c3a24dab3e69182d3ddd67a2e
Dylan Bedetti <dylanbedetti@gmail.com>
2020-12-10 02:32:22 +0000

    adding redux

--- section_20/streams/client/src/index.js
+++ section_20/streams/client/src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import App from "./components/App";
+import reducers from "./reducers";
 
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const store = createStore(reducers, composeWithDevTools());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
